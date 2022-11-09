78ada425dfb8e95010efea0a4e73fbdac4079f71
Margustova <shchennikova.ma@gmail.com>
2022-11-09 22:22:42 +0200

    Task1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+int FindSum(int number)
+{
+    int sum = 0;//объявляем переменную, хранилище для промежуточного результата
+    for(int current = 1; current <= number; current++) //объявили переменную, которая существует только в цикле for
+        sum += current; // краткая форма увеличения левого значения на правое, присваиваем sum  значение current
+    return sum;
+}
+Console.Write("Input a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sum of numbers from 1 to {a} is {FindSum(a)}");
