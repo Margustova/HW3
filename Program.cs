f5495f8cbaadafa39f621f30680289c2cfc83b29
Margustova <shchennikova.ma@gmail.com>
2022-11-10 11:56:40 +0200

    task massiv

--- Program.cs
+++ Program.cs
@@ -11,23 +11,23 @@
 // Console.WriteLine($"Sum of numbers from 1 to {a} is {FindSum(a)}");
 
 //Напишите программу, которая принимает число и выдает количество цифр в числе 
-int CountDigit(int number)
-{
-    int count = 0;
-    if (number == 0)
-    {
-        count = 1;
-    }    
-    while (number != 0)
-    {
-        number /= 10; //a/=b это деление с присваиванием, является сокращенной записью number = number/10.
-        count++;
-    }
-    return count;
-}
-Console.Write("Input a number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"The number of digit in {num} is {CountDigit(num)}");
+// int CountDigit(int number)
+// {
+//     int count = 0;
+//     if (number == 0)
+//     {
+//         count = 1;
+//     }    
+//     while (number != 0)
+//     {
+//         number /= 10; //a/=b это деление с присваиванием, является сокращенной записью number = number/10.
+//         count++;
+//     }
+//     return count;
+// }
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"The number of digit in {num} is {CountDigit(num)}");
 
 // // Напишите программу, которая принимает на вход число (А) и выдаёт произведение чисел от 1 до А.
 // int FindMulti(int number)
@@ -49,3 +49,41 @@ Console.WriteLine($"The number of digit in {num} is {CountDigit(num)}");
 // Console.Write("Input a number: ");
 // int a = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine($"Multiply of numbers from 1 to {a} is {FindMulti(a)}");
+
+/* Массив
+Метод по генерации произвольного массива
+// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.*/
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue) // размер массива, максимальное значение и минимальное
+// {
+//     int[] array = new int[size]; // выделяем память под массив- надо запомнить, имя массива (array), тип массива int, размер массива size
+    
+//     for(int i = 0; i < size; i++) //  при работе с массивом, это всегда цикл, i- индекс
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+
+//     return array; // возвращаем массив, пишем только его имя!
+
+// }
+//  void ShowArray(int[] array) // войтовский метод будем вызывать массив
+//  {
+//     Console.Write("Array is: "); // красивая финтифлюшка для вывода массива
+    
+//     for (int i =0; i < array.Length; i++) // обращаемся к длине массива
+//         Console.Write(array[i] + " ");
+
+//     Console.WriteLine(" ;) \n"); // \n  переход на новую строку
+
+//  }
+// Console.Write("Input a number of elements: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a min possible value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a max possible value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateRandomArray(n, min, max);
+// ShowArray(myArray);
+
+/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. */
+/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. */
+/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. */
