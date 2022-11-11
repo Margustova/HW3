86cdbb6cf5873eacc07d19dbf32270eaee58f8ac
Margustova <shchennikova.ma@gmail.com>
2022-11-11 13:51:52 +0200

    massive task

--- Program.cs
+++ Program.cs
@@ -109,25 +109,107 @@
 // Console.WriteLine($" The degree {num1} of  {num2} is {DegreeNums(num1, num2)}");
 
 /* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. */
-int SumDigit(int number)
-{
-    int sum = 0; // переменная отвечает за сумму
-    int a = 0; // запись последней цифры
-    while (number > 10)
-    {
-        a = number % 10; // получаем последнюю цифру числа
-        sum += a; // прибавляем каждый раз последнюю цифру (остаток)  
-        number = number / 10;   
-    } 
+// int SumDigit(int number)
+// {
+//     int sum = 0; // переменная отвечает за сумму
+//     int a = 0; // запись последней цифры
+//     while (number > 10)
+//     {
+//         a = number % 10; // получаем последнюю цифру числа
+//         sum += a; // прибавляем каждый раз последнюю цифру (остаток)  
+//         number = number / 10;   
+//     } 
     
-        if (number < 10)
-            {
-                sum = sum + number;
-            }
+//         if (number < 10)
+//             {
+//                 sum = sum + number;
+//             }
       
-    return sum;
+//     return sum;
     
+// }
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"The sum of digit of number {num} is {SumDigit(num)}");
+
+/* Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] */
+
+// int[] CreateArray(int size) // int minValue, int maxValue) // размер массива, максимальное значение и минимальное
+// {
+//     int[] array = new int[size]; // выделяем память под массив- надо запомнить, имя массива (array), тип массива int, размер массива size
+    
+//     for(int i = 0; i < size; i++) //  при работе с массивом, это всегда цикл, i- индекс
+//         array[i] = 
+
+//     return array; // возвращаем массив, пишем только его имя!
+
+// }
+//  void SetArray(int[] array) // войтовский метод будем вызывать массив
+//  {
+//     Console.Write("Array is: "); // красивая финтифлюшка для вывода массива
+    
+//     for (int i =0; i < array.Length; i++) // обращаемся к длине массива
+//         Console.Write(array[i] + " ");
+
+//     Console.WriteLine(" ;) \n"); // \n  переход на новую строку
+
+//  }
+// Console.Write("Input a size of elements: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second possible value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a max possible value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateRandomArray(n, min, max);
+// ShowArray(myArray);
+
+// Семинар 5 
+/*Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных элементов массива.*/
+int[] CreateRandomArray(int size, int minValue, int maxValue) // метод создает рандомный массив , размер массива, максимальное значение и минимальное
+{
+    int[] array = new int[size]; // выделяем память под массив- надо запомнить, имя массива (array), тип массива int, размер массива size
+    
+    for(int i = 0; i < size; i++) //  при работе с массивом, это всегда цикл, i- индекс
+        array[i] = new Random().Next(minValue, maxValue + 1);
+
+    return array; // возвращаем массив, пишем только его имя!
+
 }
-Console.Write("Input a number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"The sum of digit of number {num} is {SumDigit(num)}");
+ void ShowArray(int[] array) // войтовский метод будем вызывать массив
+ {
+    Console.Write("Array is: "); // красивая финтифлюшка для вывода массива
+    
+    for (int i =0; i < array.Length; i++) // обращаемся к длине массива
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine(" ;) \n"); // \n  переход на новую строку
+
+ }
+int GetNegativeSum(int[]array)
+{
+    int sum = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    
+        if(array[i] < 0) // сумма элементов массива меньше нуля
+            sum += array[i];
+        
+    return sum;
+}
+Console.Write("Input a number of elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(n, min, max);
+ShowArray(myArray);
+
+int negativeSum = GetNegativeSum(myArray);
+Console.WriteLine("Sum of negative elements is" + negativeSum);
